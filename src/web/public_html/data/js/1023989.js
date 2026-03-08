ed0b24115614099727384b5b49fdb1c7ee1d7de2
User <user@example.com>
2026-03-08 02:55:37 -0500

    Change to wss://

--- src/web/public_html/data/js/1023989.js
+++ src/web/public_html/data/js/1023989.js
@@ -41816,7 +41816,7 @@
       R = k;
       a &&
         (d.SetHandler("receive", Object.freeze(k)),
-        d.Connect("ws://" + (window.GAME_SERVER_URL || window.location.hostname + ":9001") + "/" + c));
+        d.Connect("wss://" + (window.GAME_SERVER_URL || window.location.hostname + ":9001") + "/" + c));
       //d.Connect("wss://server.thbound.com/" + c));
       DN = this;
       this.ds = d;
